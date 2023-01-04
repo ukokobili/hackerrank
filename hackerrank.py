e3ccad479f39ef4288af83e4c5d02d222f1f06d6
ukokobili <ukokobili.jacob@gmail.com>
2023-01-04 04:09:56 +0100

    code update

--- hackerrank.py
+++ hackerrank.py
@@ -42,3 +42,66 @@ def is_leap(year):
 
 year = int(input())
 print(is_leap(year))
+
+"""
+The included code stub will read an integer, n, from STDIN.
+
+Without using any strings methods, try to print the following:
+
+123……..n
+
+Note that “…..” represents the consecutive values in between.
+
+Example
+
+n = 5
+Print the string 12345.
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1):
+        print(i, end="")
+
+"""
+You are given a string, and you have to validate whether it’s a valid Roman numeral. If it is valid, print True. Otherwise, print False. Try to create a regular expression for a valid Roman numeral.
+
+Input Format
+A single line of input containing a string of Roman characters.
+
+Output Format
+Output a single line containing True or False according to the instructions above.
+
+Constraints
+The number will be between 1 and 3999 (both included).
+"""
+regex_pattern = r'M{0,3}(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[VX]|V?I{0,3})$'    # Do not delete 'r'.
+
+import re
+print(str(bool(re.match(regex_pattern, input()))))
+
+"""
+Let’s dive into the interesting topic of regular expressions! You are given some input, and you are required to check whether they are valid mobile numbers.
+
+A valid mobile number is a ten digit number starting with a  or .
+
+Concept
+
+A valid mobile number is a ten digit number starting with a  or .
+
+Regular expressions are a key concept in any programming language.
+
+Input Format
+The first line contains an integer N, the number of inputs.
+N lines follow, each containing some string.
+
+Constraints
+1 <= N <= 10
+2 <= len(Number) <= 15
+Output Format
+For every string listed, print “YES” if it is a valid mobile number and “NO” if it is not on separate lines. Do not print the quotes.
+"""
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import re
+[print('YES' if re.match(r'[789]\d{9}$', input()) else 'NO') for _ in range(int(input()))]
